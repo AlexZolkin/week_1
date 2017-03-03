4a3c7d9619c17a7d994cceae1b08011daca3cc13
alexzolkin19 <alexzolkin19@gmail.com>
2017-03-03 16:24:53 +0300

    Overriding equals()

---
+++ Equals/Student.java
@@ -0,0 +1,7 @@
+package week_1.Equals;
+
+/**
+ * Created by Алексей on 03.03.2017.
+ */
+public class Student {
+}
---
+++ Equals/University.java
@@ -0,0 +1,9 @@
+package week_1.Equals;
+
+import week_1.Abstraction_Interfaces.Books_Task.Edition;
+
+/**
+ * Created by Алексей on 03.03.2017.
+ */
+public class University implements Entry {
+}
